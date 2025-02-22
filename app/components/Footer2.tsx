2f920da9a32833442aef87c6ab534042bfb84195
Abby Milberg <abby805@gmail.com>
2025-02-21 22:05:37 -0500

    uncommenting footer contents since changing the file name seemed to work

--- app/components/Footer2.tsx
+++ app/components/Footer2.tsx
@@ -1,52 +1,51 @@
-// import Link from 'next/link'
+import Link from 'next/link'
 
-// import Row from './layout/Row';
-// import footerStyles from '../styles/footer.module.css'
+import Row from './layout/Row';
+import footerStyles from '../styles/footer.module.css'
 
-// type FooterLink = {
-//   href: string
-//   name: string
-// }
+type FooterLink = {
+  href: string
+  name: string
+}
 
-// const footerItems = [
-//   {
-//     href: '/rss',
-//     name: 'RSS',
-//   },
-//   // {
-//   //   href: 'https://www.linkedin.com/in/abigailmilberg/',
-//   //   name: 'LinkedIn',
-//   // },
-//   // {
-//   //   href: 'https://www.drupal.org/u/abbym',
-//   //   name: 'Drupal.org',
-//   // },
-// ]
+const footerItems = [
+  {
+    href: '/rss',
+    name: 'RSS',
+  },
+  {
+    href: 'https://www.linkedin.com/in/abigailmilberg/',
+    name: 'LinkedIn',
+  },
+  {
+    href: 'https://www.drupal.org/u/abbym',
+    name: 'Drupal.org',
+  },
+]
 
 const Footer = () => {
   return (
-    <p>Footer2 here</p>
-    // <Row>
-    //   <footer className={footerStyles.footer}>
-    //     <ul>
-    //       {footerItems.map((item: FooterLink) => {
-    //         return (
-    //           <li key={`footer-${item.href}`}>
-    //             <Link
-    //               href={item.href}
-    //               rel={'noopener noreferrer'}
-    //             >
-    //               {item.name}
-    //             </Link>
-    //           </li>
-    //         )
-    //       })}
-    //     </ul>
-    //     <p>
-    //       © {new Date().getFullYear()} Abby Milberg
-    //     </p>
-    //   </footer>
-    // </Row>
+    <Row>
+      <footer className={footerStyles.footer}>
+        <ul>
+          {footerItems.map((item: FooterLink) => {
+            return (
+              <li key={`footer-${item.href}`}>
+                <Link
+                  href={item.href}
+                  rel={'noopener noreferrer'}
+                >
+                  {item.name}
+                </Link>
+              </li>
+            )
+          })}
+        </ul>
+        <p>
+          © {new Date().getFullYear()} Abby Milberg
+        </p>
+      </footer>
+    </Row>
   )
 }
 
