758ba05c1e07438548797a7f37d8cca49ed15d89
Abby Milberg <abby805@gmail.com>
2025-02-21 21:59:20 -0500

    debugging footer

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -1,51 +1,52 @@
-import Link from 'next/link'
+// import Link from 'next/link'
 
-import Row from './layout/Row';
-import footerStyles from '../styles/footer.module.css'
+// import Row from './layout/Row';
+// import footerStyles from '../styles/footer.module.css'
 
-type FooterLink = {
-  href: string
-  name: string
-}
+// type FooterLink = {
+//   href: string
+//   name: string
+// }
 
-const footerItems = [
-  {
-    href: '/rss',
-    name: 'RSS',
-  },
-  // {
-  //   href: 'https://www.linkedin.com/in/abigailmilberg/',
-  //   name: 'LinkedIn',
-  // },
-  // {
-  //   href: 'https://www.drupal.org/u/abbym',
-  //   name: 'Drupal.org',
-  // },
-]
+// const footerItems = [
+//   {
+//     href: '/rss',
+//     name: 'RSS',
+//   },
+//   // {
+//   //   href: 'https://www.linkedin.com/in/abigailmilberg/',
+//   //   name: 'LinkedIn',
+//   // },
+//   // {
+//   //   href: 'https://www.drupal.org/u/abbym',
+//   //   name: 'Drupal.org',
+//   // },
+// ]
 
 const Footer = () => {
   return (
-    <Row>
-      <footer className={footerStyles.footer}>
-        <ul>
-          {footerItems.map((item: FooterLink) => {
-            return (
-              <li key={`footer-${item.href}`}>
-                <Link
-                  href={item.href}
-                  rel={'noopener noreferrer'}
-                >
-                  {item.name}
-                </Link>
-              </li>
-            )
-          })}
-        </ul>
-        <p>
-          © {new Date().getFullYear()} Abby Milberg
-        </p>
-      </footer>
-    </Row>
+    <p>Footer here</p>
+    // <Row>
+    //   <footer className={footerStyles.footer}>
+    //     <ul>
+    //       {footerItems.map((item: FooterLink) => {
+    //         return (
+    //           <li key={`footer-${item.href}`}>
+    //             <Link
+    //               href={item.href}
+    //               rel={'noopener noreferrer'}
+    //             >
+    //               {item.name}
+    //             </Link>
+    //           </li>
+    //         )
+    //       })}
+    //     </ul>
+    //     <p>
+    //       © {new Date().getFullYear()} Abby Milberg
+    //     </p>
+    //   </footer>
+    // </Row>
   )
 }
 
