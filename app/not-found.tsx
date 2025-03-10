2205bf06206bc77be1c460b89e500975a5fe7739
Abby Milberg <abby805@gmail.com>
2025-03-09 21:09:21 -0400

    OG images and 404 page

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,10 +1,13 @@
+import Row from 'app/components/Row'
+import rowStyles from 'app/styles/row.module.css'
+
 export default function NotFound() {
   return (
-    <section>
-      <h1>
-        404 - Page Not Found
-      </h1>
-      <p>The page you are looking for does not exist.</p>
-    </section>
+    <Row color="black" collapsePadTop>
+      <div className={rowStyles['col-md-8']}>
+        <h1>404: Page not found</h1>
+        <p>Not <em>all</em> who wander are lost... 😛</p>
+      </div>
+    </Row>
   )
 }
